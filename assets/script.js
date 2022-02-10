bccba80a464ee16ea221e544c346fd74627219cb
Nicholas Amideo <namideo@Nicholass-MacBook-Pro.local>
2022-02-09 23:29:29 -0500

    added api

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,22 @@
 var API = ebba861ab242756d28cc03436e29409c;
 
-var cityEl = document.getElementById("#City"); 
-var dateEl = document.querySelector('#Date');
-var tempEl = document.querySelector('#Temp');
-var windEl = document.querySelector('#Wind');
-var humidEl = document.querySelector('#Humidity');
-var uvIndexEl = document.querySelector('#UVindex');
+var searchFormEl = $('#search-form');
+var city = $("#City"); 
+var date = $('#Date');
+var temp = $('#Temp');
+var wind = $('#Wind');
+var humid = $('#Humidity');
+var uvIndex = $('#UVindex');
 
 
-var searchFormEl = document.querySelector('#search-form');
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API;
 
 
-function getApi() {
-
-  var requestUrl = "https://openweathermap.org/current#name";
-
-  fetch(requestUrl)
+  fetch(queryURL)
     .then(function (response) {
       return response.json();
     })
     
-}
 
 
 
-
-
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
-}
-
-searchFormEl.addEventListener('click', getApi);
